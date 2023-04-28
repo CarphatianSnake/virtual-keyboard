178a471eb136f162b1e7e132e0c76233c9de232d
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-04-28 13:38:03 +0300

    refactor: refactor renderFooter function and add hotkey for select all text

--- src/renderApp/renderFooter.js
+++ src/renderApp/renderFooter.js
@@ -1,26 +1,19 @@
+import footerData from '../../assets/data/footerData.js';
+
 function renderFooter(parent) {
   const language = window.localStorage.getItem('lang');
-
   const footer = document.createElement('footer');
   footer.classList.add('footer');
 
-  const os = document.createElement('p');
-  if (language === 'en') {
-    os.textContent = 'Keyboard was implemented on Windows OS base';
+  function renderFooterElements(data) {
+    data.forEach((content) => {
+      const element = document.createElement('p');
+      element.textContent = content[language];
+      footer.append(element);
+    });
   }
-  if (language === 'ua') {
-    os.textContent = 'Клавіатура реалізована на базі ОС Windows';
-  }
-  footer.append(os);
 
-  const langChange = document.createElement('p');
-  if (language === 'en') {
-    langChange.textContent = 'To change keyboard language use Ctrl + Shift combination or click on language flag';
-  }
-  if (language === 'ua') {
-    langChange.textContent = 'Для зміни мови клавіатури скористайтеся комбінацієй Ctrl + Shift, або клікніть на прапорець мови';
-  }
-  footer.append(langChange);
+  renderFooterElements(footerData);
 
   parent.append(footer);
 }
